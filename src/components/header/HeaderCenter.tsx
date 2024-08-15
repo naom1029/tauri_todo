65ca0716449c3ec37f225687be2221a5d4082df6
naom1029 <bluesky.97102945@gmail.com>
2024-08-15 15:54:11 +0900

    ヘッダーの追加

--- src/components/header/HeaderCenter.tsx
+++ src/components/header/HeaderCenter.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HeaderCenter: React.FC = () => {
-  return <div>Header Center Content</div>;
+  return <div className="header-center">Todo-アプリ</div>;
 };
 
 export default HeaderCenter;
--- src/components/header/HeaderRight.tsx
+++ src/components/header/HeaderRight.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const HeaderRight: React.FC = () => {
-  return <div>Header Light Content</div>;
+  return (
+    <div className="header-right">
+      <a href="#settings"><span className="header-icon">⚙️</span>設定</a>
+      <a href="#profile"><span className="header-icon">👤</span>プロフィール</a>
+    </div>
+  );
 };
 
 export default HeaderRight;
