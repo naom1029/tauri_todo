b8cb08972c45e33d552fe744a8806a679ef72aed
naom1029 <bluesky.97102945@gmail.com>
2024-08-17 06:26:18 +0900

    capabilitiesを必要最小限に修正

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -30,15 +30,7 @@ pub fn run() {
                 .add_migrations("sqlite:sqlite.db", migrations)
                 .build(),
         )
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_os::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
